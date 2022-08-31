6f0b754255ca7c4b54f5ec42fba7007b119f4a9f
Stephen Boyd <sboyd@kernel.org>
2022-08-31 10:53:25 -0700

    Revert "clk: core: Honor CLK_OPS_PARENT_ENABLE for clk gate ops"
    [ Upstream commit abb5f3f4b1f5f0ad50eb067a00051d3587dec9fb ]
    
    This reverts commit 35b0fac808b95eea1212f8860baf6ad25b88b087. Alexander
    reports that it causes boot failures on i.MX8M Plus based boards
    (specifically imx8mp-tqma8mpql-mba8mpxl.dts).
    
    Reported-by: Alexander Stein <alexander.stein@ew.tq-group.com>
    Cc: Chen-Yu Tsai <wenst@chromium.org>
    Fixes: 35b0fac808b9 ("clk: core: Honor CLK_OPS_PARENT_ENABLE for clk gate ops")
    Link: https://lore.kernel.org/r/12115951.O9o76ZdvQC@steina-w
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Link: https://lore.kernel.org/r/20220831175326.2523912-1-sboyd@kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -221,9 +221,6 @@ static bool clk_core_rate_is_protected(struct clk_core *core)
 	return core->protect_count;
 }
 
-static int clk_core_prepare_enable(struct clk_core *core);
-static void clk_core_disable_unprepare(struct clk_core *core);
-
 static bool clk_core_is_prepared(struct clk_core *core)
 {
 	bool ret = false;
@@ -236,11 +233,7 @@ static bool clk_core_is_prepared(struct clk_core *core)
 		return core->prepare_count;
 
 	if (!clk_pm_runtime_get(core)) {
-		if (core->flags & CLK_OPS_PARENT_ENABLE)
-			clk_core_prepare_enable(core->parent);
 		ret = core->ops->is_prepared(core->hw);
-		if (core->flags & CLK_OPS_PARENT_ENABLE)
-			clk_core_disable_unprepare(core->parent);
 		clk_pm_runtime_put(core);
 	}
 
@@ -276,13 +269,7 @@ static bool clk_core_is_enabled(struct clk_core *core)
 		}
 	}
 
-	if (core->flags & CLK_OPS_PARENT_ENABLE)
-		clk_core_prepare_enable(core->parent);
-
 	ret = core->ops->is_enabled(core->hw);
-
-	if (core->flags & CLK_OPS_PARENT_ENABLE)
-		clk_core_disable_unprepare(core->parent);
 done:
 	if (core->dev)
 		pm_runtime_put(core->dev);
@@ -959,9 +946,6 @@ int clk_rate_exclusive_get(struct clk *clk)
 }
 EXPORT_SYMBOL_GPL(clk_rate_exclusive_get);
 
-static int clk_core_enable_lock(struct clk_core *core);
-static void clk_core_disable_lock(struct clk_core *core);
-
 static void clk_core_unprepare(struct clk_core *core)
 {
 	lockdep_assert_held(&prepare_lock);
@@ -985,9 +969,6 @@ static void clk_core_unprepare(struct clk_core *core)
 
 	WARN(core->enable_count > 0, "Unpreparing enabled %s\n", core->name);
 
-	if (core->flags & CLK_OPS_PARENT_ENABLE)
-		clk_core_enable_lock(core->parent);
-
 	trace_clk_unprepare(core);
 
 	if (core->ops->unprepare)
@@ -996,9 +977,6 @@ static void clk_core_unprepare(struct clk_core *core)
 	clk_pm_runtime_put(core);
 
 	trace_clk_unprepare_complete(core);
-
-	if (core->flags & CLK_OPS_PARENT_ENABLE)
-		clk_core_disable_lock(core->parent);
 	clk_core_unprepare(core->parent);
 }
 
@@ -1047,9 +1025,6 @@ static int clk_core_prepare(struct clk_core *core)
 		if (ret)
 			goto runtime_put;
 
-		if (core->flags & CLK_OPS_PARENT_ENABLE)
-			clk_core_enable_lock(core->parent);
-
 		trace_clk_prepare(core);
 
 		ret = clk_vote_rate_vdd(core, core->rate);
@@ -1067,9 +1042,6 @@ static int clk_core_prepare(struct clk_core *core)
 			ret = core->ops->prepare(core->hw);
 
 		trace_clk_prepare_complete(core);
-
-		if (core->flags & CLK_OPS_PARENT_ENABLE)
-			clk_core_disable_lock(core->parent);
 
 		if (ret)
 			goto unprepare;
